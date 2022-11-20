0f801e26d4d65b9a65349163030fab61c034ff6f
Mayqiqi <yingqi@kis-MacBook-Pro.local>
2022-11-20 03:24:38 -0800

    update

--- app.js
+++ app.js
@@ -8,10 +8,17 @@ import UserController
 
 import TuitsController
   from "./controllers/tuits/tuits-controller.js";
+import path  from 'path';
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
 
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+console.log('directory-name 👉️', __dirname);
 const app = express()
 app.use(cors())
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 TuitsController(app);
 HelloController(app)
 UserController(app)
